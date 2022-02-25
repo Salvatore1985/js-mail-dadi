a952721ab02452ea82fccf3343a826a65b02a28d
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-02-25 14:35:47 +0100

    LEGO UN EVENTO AL CLICK DI UN BUTTON

--- js/script.js
+++ js/script.js
@@ -8,24 +8,28 @@ e non usando funzionalità built-in degli array in js :)
  */
 
 //* RECUPERO GLI ELEMENTI DEL FORM
-
 const emailElement = document.getElementById("my-email");
 /* console.log("emailElement"); */
+const buttonEnter = document.getElementById("enter");
 
-/*Creo un array di Email*/
+//*Creo un array di Email*/
 const userEmail = ["salvo@gmail", "franco@gmail", "luigi@gmail"];
 /* console.table(userEmail); */
 
-//? RECUPERO I VALORI DEL FOR EMAIL
-let emailForm = emailElement.value;
-console.log(emailForm);
+
+//*LEGO UN EVENTO AL CLICK DI UN BUTTON
+buttonEnter.addEventListener("click", function () {
+    //? RECUPERO I VALORI DEL FOR EMAIL
+    let emailForm = emailElement.value;
+    console.log(emailForm);
+
+    // ! CONVALIDA DATI CON UN CICLO FOR
+    for (let i = 0; i < userEmail.length; i++) {
+        const emailCurrent = userEmail[i];
+        console.log(emailCurrent);
+
+    }
+
+})
 
 
-
-
-// ! CONVALIDA DATI CON UN CICLO FOR
-for (let i = 0; i < userEmail.length; i++) {
-    const emailCurrent = userEmail[i];
-    console.log(emailCurrent);
-
-}
