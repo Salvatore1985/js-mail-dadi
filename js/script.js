a0032385664aa00f770ce3dfe745c5d336265f51
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-02-25 14:16:39 +0100

    RECUPERO GLI ELEMENTI DEL FORM

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/**
+* * Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Note: il controllo vorrei che fosse effettuato manualmente con un ciclo for 
+e non usando funzionalità built-in degli array in js :)
+ */
+
+//* RECUPERO GLI ELEMENTI DEL FORM
+
+const emailElement = document.getElementById("my-email");
+console.log("emailElement");
+
+/*Creo un array di Email*/
+const userEmail = ["salvo@gmail", "franco@gmail", "luigi@gmail"];
+console.table(userEmail);
+
+// ! CONVALIDA DATI CON UN CICLO FOR
